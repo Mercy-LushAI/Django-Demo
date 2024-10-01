2142ca1ff4ddeee247c51214f457ff2b7fbdbc86
mercy <mercy.lalthangmawii@lushaitech.com>
2024-10-01 17:01:32 +0530

    added models

--- base/models.py
+++ base/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Room(models.Models):
+    # host = 
+    # topic = 
+    name = models.CharField(max_length = 200)
+    description = models.TextField(null = True, blank = True)
+    # participants = 
+    updated = models.DateTimeField(auto_now = True)
+    created = models.DateTimeField(auto_now_add = True)
+
+    def _str__(self):
+        return self.name
